bed33153a02b195f344a45f56e4ac966b39b518d
Empoderada <raccooneve@gmail.com>
2020-02-06 12:59:36 -0700

    Envio de datos en el formulario, no sirve

--- practica_1/vue_practica/src/components/LoginForm.vue
+++ practica_1/vue_practica/src/components/LoginForm.vue
@@ -7,48 +7,85 @@
         <h4 class="mb-5">img</h4>
         <h3>Login</h3>
       <div class="form-group">
-              <div>
                   <input
                     type="email"
                     class="mb-3 mt-3"
                     placeholder="Correo electrónico"
+                    v-model="user.email"
                   >
-              </div>
-              <div>
                   <input
                   type="text"
                   class="mb-3 mt-3"
                   placeholder="Nombre"
                   />
-              </div>
-              <div>
                   <input
                     type="password"
                     class="mb-3 mt-3"
                     placeholder="Contraseña"
                   />
-                </div>
+                  <!--Handlebars templating, pasar cosas como js a html-->
+                  <!--{{user.password}}-->
       </div>
     <div class="row">
       <div class="col">
       </div>
-      <div class="col">
-        <p>Olvidaste la contraseña?</p>
+      <div class="col pb-5">
+        <small>¿Olvidaste la contraseña?</small>
       </div>
     </div>
 
-        <button class="btn-purple btn-block">Login</button>
+        <button
+          class="btn-purple btn-block"
+          @click="login"
+          >Login</button>
       </section>
     </section>
 </template>
 
 <script lang="js">
-export default {
-  name: 'LoginForm'
+export default { // febrero 06 2020
+  name: 'LoginForm',
+  data () { // poner los datos que estan siendo ingresados en el momento
+    return {
+      user: {
+        email: '',
+        password: ''
+      },
+      userEmail: '',
+      userPassword: ''
+    }
+  },
+  beforeCreate () {
+    console.log('Antes de ser creado')
+  },
+  created () {
+    console.log('Estoy en created')
+  },
+  mounted () { // Siempre vamos a usar este pare ejecutar cualquier funcion preparatoria
+    console.log('Estoy en mounted')
+  },
+  methods: {
+    // metodos que queramos utilizar solo en este archivo
+    login () {
+      let user = {
+        email: 'esto es local'
+      }
+      console.log('soy el login')
+      console.log('User local' + user)
+      console.log(this.user.email)
+      console.log(this.user.password)
+
+      setTimeout(() => {
+        // Luego de iniciar sesion nos envia a la pagina about
+        this.$router.push({ name: 'about' })
+      }, 1000)
+    }
+  }
 }
 </script>
 
 <script lang="scss">
+// style, ubiamos el css relacionado a nuestros views y componentes
 </script>
 
 <style lang="scss">
