438bf706dd4cf1e0af4c87ce2329bd129a024eaf
deltallye <aneira_bengalski@hotmail.com>
2020-03-10 11:47:26 -0700

    Pantalla de jugador, tiene errores, mejorar

--- practica_1/Inicio_sesion/src/main.js
+++ practica_1/Inicio_sesion/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import Auth from './config/auth'
 import 'animate.css'
+import { firestorePlugin } from 'vuefire'
 
 // Librerias de usuario FEB 04 2020}
 // Con esto se cambia el estilo de la pagina
--- practica_1/Inicio_sesion/src/views/Juego/Partida.vue
+++ practica_1/Inicio_sesion/src/views/Juego/Partida.vue
@@ -5,26 +5,81 @@
         <h3 class="text-center">Juego 1</h3>
       </div>
     </div>
+    <div class="centrar-row">
+        <button
+        class="btn btn-success"
+        @click="crearPartida"
+        >
+          Nueva partida
+        </button>
+      </div>
     <div class="centrar-row partida contenido container">
       <div class="col col-sm-6">
-        <user-arena @opcion="getOpcion"></user-arena>
+        <user-arena 
+        @opcion="getOpcion" 
+        :userOpcion="partida.usuario_1"></user-arena>
       </div>
       <div class="col col-sm-6">
-        <user-arena></user-arena>
+        <user-arena :userOpcion)"partida.usuario_1 != ´´ ?"></user-arena>
       </div>
     </div>
   </section>
 </template>
 <script lang="js">
 import UserArena from '@/components/Juego/UserArena'
+import fireApp from '@/config/_firebase.js'
+const partida users = fireApp,firestore().collection('users')
 export default {
   name: 'Partida',
   components: {
     UserArena
   },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.obtenerPArtida(to.params)
+    })
+  },
+  data () {
+    return {
+      partida: ''
+    }
+  },
+  firestore: {
+    partida: fireApp.firestore.collection('juego-1')
+  },
+  // Hwlpwe para asignar objetos o variables que necesitan ser detectados en sus cambios para ejecutar metodos
+  watch: {
+    '$route.params': {
+      deep: true, // puede ser un objeto anidado, fijarnos en el ultimojson
+      immediate: true, // no esperar
+      handler (value) { // lo que vas a hacer cuando detectes un ca,bio
+        this.$bind('user', partida.doc(value.no_partida))
+      }
+    }
+  },
   methods: {
+    // ;etodo paragenerar nueva partida
+    crearPartida () { // Toma los datos de la base de datos de firebase
+    // escribe en la base de datos
+      fireApp.firestore().collection('juego-1').doc('partida').set({
+        'usuario-1': '',
+        'usuario-2': '',
+        'ganador': ''
+      })
+    },
+    // cargar los datos de la partida del firestore
+    obtenerPArtida (partida) {
+      fireApp.firestore().collection('juego-1').doc(partida).get().then((result) => {
+        console.log(result.data)
+      })
+    },
     getOpcion (opcion) {
-      alert(`Estoy en partida: ${opcion}`)
+      //alert(`Estoy en partida: ${opcion}`)
+      fireApp.firestore().collection('juego-1'),doc(this.$route.params.no_partida).update({
+        'usuario-1': '',
+        'usuario-2': '',
+        'ganador': ''
+      })
     }
   }
 }
